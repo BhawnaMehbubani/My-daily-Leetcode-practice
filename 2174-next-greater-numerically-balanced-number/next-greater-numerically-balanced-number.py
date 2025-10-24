3df697e6876bb2c35dd0380042bfdf0333336734
Bhawna Mehbubani <bhawnamehbubani@gmail.com>
2025-10-24 21:56:20 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (64.53%) - LeetSync

--- 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.py
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.py
@@ -1,3 +1,4 @@
+'''
 class Solution:
   def nextBeautifulNumber(self, n: int) -> int:
     def isBalance(num: int) -> bool:
@@ -13,3 +14,32 @@ class Solution:
     while not isBalance(n):
       n += 1
     return n
+
+'''
+all_perms = []
+for i in range(1, 8):
+    curr_list = [i]*i
+    curr_perms = list(set(permutations(curr_list)))
+    all_perms.extend(curr_perms)
+    if i == 1:
+        curr_list = [1, 2, 2, 3, 3, 3]
+        curr_perms = list(set(permutations(curr_list)))
+        all_perms.extend(curr_perms)
+        curr_list = [1, 2, 2, 4,4,4,4]
+        curr_perms = list(set(permutations(curr_list)))
+        all_perms.extend(curr_perms)
+    for j in range(1, i//2+1):
+        a, b = j, i-j
+        if a == b:
+            break
+        curr_list = [a]*a + [b]*b
+        curr_perms = list(set(permutations(curr_list)))
+        all_perms.extend(curr_perms)
+all_perms = [int(''.join(str(a) for a in num)) for num in all_perms]
+all_perms.sort()
+
+class Solution:
+    def nextBeautifulNumber(self, n: int) -> int:
+        for num in all_perms:
+            if num > n:
+                return num
